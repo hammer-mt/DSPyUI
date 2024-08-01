2d9e44cc816f743eb408b02cf4297ddd81b4b5bc
Mike Taylor <mjt145@gmail.com>
2024-08-01 20:10:57 +0100

    finally got a basic signature thing working

--- test.py
+++ test.py
@@ -3,11 +3,6 @@ import gradio as gr
 with gr.Blocks() as demo:
     input_count = gr.State(1)
     output_count = gr.State(1)
-    add_input_btn = gr.Button("Add Input")
-    add_output_btn = gr.Button("Add Output")
-
-    add_input_btn.click(lambda count: count + 1, input_count, input_count)
-    add_output_btn.click(lambda count: count + 1, output_count, output_count)
 
     @gr.render(inputs=[input_count, output_count])
     def render_tracks(input_count, output_count):
@@ -15,34 +10,36 @@ with gr.Blocks() as demo:
         outputs = []
         with gr.Row():
             with gr.Column():
+                gr.Markdown("### Inputs")
                 for i in range(input_count):
                     with gr.Group():
-                        input = gr.Textbox(placeholder="Input", key=f"input-{i}", show_label=False)
+                        input = gr.Textbox(placeholder="Input{i}", key=f"input-{i}", show_label=False)
                         inputs.append(input)
             
             with gr.Column():
+                gr.Markdown("### Outputs")
                 for i in range(output_count):
                     with gr.Group():
-                        output = gr.Textbox(placeholder="Output", key=f"output-{i}", show_label=False)
+                        output = gr.Textbox(placeholder="Output{i}", key=f"output-{i}", show_label=False)
                         outputs.append(output)
 
         def compile(data):
-            input_text = ""
-            for input in inputs:
-                input_val = data[input]
-                input_text += input_val + ", "
-            
-            output_text = ""
-            for output in outputs:
-                output_val = data[output]
-                output_text += output_val + ", "
+            input_text = ", ".join(data[input] for input in inputs if data[input])
+            output_text = ", ".join(data[output] for output in outputs if data[output])
+            result = f"{input_text} -> {output_text}"
+            return result
 
-            return input_text, output_text
+        submit_btn.click(compile, set(inputs + outputs), final_result)
 
-        submit_btn.click(compile, set(inputs + outputs), [input_result, output_result])
+    with gr.Row():
+        with gr.Column():
+            add_input_btn = gr.Button("Add Input")
+            add_input_btn.click(lambda count: count + 1, input_count, input_count)
+        with gr.Column():
+            add_output_btn = gr.Button("Add Output")
+            add_output_btn.click(lambda count: count + 1, output_count, output_count)
 
-    submit_btn = gr.Button("Print Inputs and Outputs")
-    input_result = gr.Textbox(label="Input Result", interactive=False)
-    output_result = gr.Textbox(label="Output Result", interactive=False)
+    submit_btn = gr.Button("Print Signature")
+    final_result = gr.Textbox(label="Final Result", interactive=False)
 
 demo.launch()
