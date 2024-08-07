f823e84d30438925a62410707b9ee8691f485edc
Mike Taylor <mjt145@gmail.com>
2024-08-07 16:37:25 +0100

    updated todos

--- core.py
+++ core.py
@@ -113,6 +113,7 @@ def compile_program(input_fields: List[str], output_fields: List[str], dspy_modu
     # Set up the optimizer
     if optimizer == "BootstrapFewShot":
         teleprompter = BootstrapFewShot(metric=metric, teacher_settings=dict(lm=teacher_lm))
+    # TODO: fix the no lm loaded issue
     elif optimizer == "BootstrapFewShotWithRandomSearch":
         teleprompter = BootstrapFewShotWithRandomSearch(metric=metric, teacher_settings=dict(lm=teacher_lm))
     elif optimizer == "MIPRO":
